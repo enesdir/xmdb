7f0db0b2658b96e2277990ed1ffa71a15f239c9f
Enes Esen <enesen@gmx.com>
2023-07-18 02:10:14 +0300

    feat: mobile menu added to header

--- src/features/layout/components/Header/Header.tsx
+++ src/features/layout/components/Header/Header.tsx
@@ -1,32 +1,31 @@
 'use client'
 
-import Link from 'next/link'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { AccessControl, Button, Container } from '@/components/'
-import { PROJECT_NAME } from '@/lib/constants'
 import { LanguageMenu } from '../LanguageMenu/LanguageMenu'
 import { AuthButtons } from './AuthButtons/AuthButtons'
+import { Logo } from './Logo'
+import { MobileMenu } from './MobileMenu'
+import { MobileSearch } from './MobileSearch'
 import { SearchBar } from './SearchBar'
 import { UserMenu } from './UserMenu/UserMenu'
 
 export const Header = () => (
-	<header className='h-16 bg-[#121212] shadow-md shadow-gray-200/30'>
-		<Container className='flex h-full items-center justify-between gap-x-1'>
-			<div className='order-1 items-center rounded-md bg-[#f5c518]'>
-				<Link href='/' className='p-1 text-2xl font-extrabold text-gray-900'>
-					{PROJECT_NAME}
-				</Link>
-			</div>
+	<header className='relative z-[1000] m-0 flex min-h-[3.5rem] w-full items-center border-none bg-[rgb(18,18,18)] p-1 text-base shadow-md shadow-gray-200/30'>
+		<Container className='flex h-full items-center justify-between  px-0 sm:gap-x-1 '>
+			<MobileMenu />
+			<Logo />
 			<Button
 				icon={<AiOutlineMenu className='text-2xl leading-5' />}
 				variant='text'
-				className='order-2'
+				className='order-3 hidden'
 				onClick={() => alert('click')}
 			>
 				Menu
 			</Button>
+			<MobileSearch />
 			<SearchBar />
-			<div className='order-4'>
+			<div className='order-5'>
 				<AccessControl renderNoAccess={<AuthButtons />} permissions={['isLoggedIn']}>
 					<UserMenu />
 				</AccessControl>
