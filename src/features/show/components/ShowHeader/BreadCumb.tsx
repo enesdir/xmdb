ded11ebffd314e168614d0bff63bad919d0187b3
Enes Esen <enesen@gmx.com>
2023-07-19 01:54:32 +0300

    feat: added breadcumb and videoplayer

---
+++ src/features/show/components/ShowHeader/BreadCumb.tsx
@@ -0,0 +1,41 @@
+import { BsFillShareFill } from 'react-icons/bs'
+import { TbListSearch } from 'react-icons/tb'
+import { Button, IconButton } from '@/components'
+import { BaseLink } from '@/components/BaseLink'
+import { List, ListItem } from '@/components/List'
+
+export const BreadCumb = () => {
+	return (
+		<div className='flex h-12 items-center justify-between'>
+			<div className='flex flex-nowrap items-center'></div>
+			<div className='flex flex-nowrap items-center'>
+				<List className='hidden text-sm font-medium text-white md:inline-flex'>
+					<ListItem>Cast & Crew</ListItem>
+					<ListItem className='before:mx-2 before:mb-[0.2rem] before:mt-0 before:inline-block before:rounded-[50%] before:bg-current before:p-px before:align-middle before:text-base before:leading-[0.5rem] before:content-[""]'>
+						Trivia
+					</ListItem>
+				</List>
+				<div className='hidden before:mr-3 before:h-6 before:w-[1px] before:bg-white/20 before:content-[""] after:ml-3 after:h-6 after:w-[1px] after:bg-white/20 after:content-[""] md:inline-flex'>
+					<BaseLink href='#' label='Link' className='text-white'>
+						XMDbPro
+					</BaseLink>
+				</div>
+				<Button
+					icon={<TbListSearch className='text-2xl' />}
+					variant='text'
+					className='m-0 inline-flex min-h-[2rem] flex-row flex-nowrap content-center px-4 py-0 -tracking-tight text-white'
+				>
+					<span className='hidden sm:inline-block'>All topics</span>
+				</Button>
+				<div className='inline-block before:absolute before:mt-3 before:h-6 before:w-px before:bg-white/20 before:content-[""]'>
+					<IconButton
+						icon={<BsFillShareFill className='text-xl' />}
+						label='Share on social media'
+						className='text-white'
+						variant='rounded'
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
---
+++ src/features/show/components/ShowHeader/Player.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { BsThreeDotsVertical } from 'react-icons/bs'
+import { IconButton } from '@/components'
+
+export const Player = ({ show }) => {
+	return (
+		<div className='relative mr-0 inline-flex w-full min-w-[auto] cursor-pointer flex-col sm:ml-1 sm:w-[calc(72.35%_-_0.125rem)] sm:bg-none sm:pb-0 lg:mr-1 lg:w-[calc(60%_-_0.125rem)]'>
+			<div className='absolute right-2.5 top-2.5 z-10'>
+				<IconButton
+					variant='rounded'
+					label='Video Preferences'
+					icon={<BsThreeDotsVertical />}
+					className='bg-[rgba(18,18,18,0.45)] p-2 text-white'
+				/>
+			</div>
+			<div className='relative mr-0 inline-flex w-full min-w-[auto] cursor-pointer bg-[rgb(18,18,18)] pb-14 sm:bg-none sm:pb-0'>
+				<div
+					className='relative inline-flex w-full min-w-[auto] overflow-hidden text-white/10 before:relative before:block before:w-full before:bg-white/10 before:pb-[56.25%] before:content-[""]'
+					style={{ width: '100%' }}
+				>
+					<Image
+						alt='Home Video Trailer from Sony Pictures Home Entertainment'
+						className='object-cover'
+						src={show.images[0]}
+						sizes='100vw, (min-width: 480px) 68vw, (min-width: 600px) 52vw, (min-width: 1024px) 32vw, (min-width: 1280px) 32vw'
+						fill
+					/>
+				</div>
+				<div className='invisible absolute left-0 top-0 h-full w-full opacity-0 transition-opacity delay-300'></div>
+				<Link
+					href='#'
+					className='absolute inset-0 box-border flex h-full w-full items-end p-2 text-sm font-normal normal-case leading-5 tracking-[0.01786em] text-white'
+					aria-label={`Watch ${show.title}`}
+				></Link>
+			</div>
+		</div>
+	)
+}
