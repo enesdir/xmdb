e58fb81229854ef5f8623190daed8b76585f2003
Enes Esen <enesen@gmx.com>
2023-07-25 11:05:09 +0300

    feat: able to test react hooks

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,12 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	plugins: [react()],
 	test: {
+		globals: true,
 		environment: 'jsdom',
 	},
 })
