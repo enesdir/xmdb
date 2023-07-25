1c8857712a53a975feee8084016d2106a5bf3b07
Enes Esen <enesen@gmx.com>
2023-07-25 11:05:32 +0300

    feat: added useboolean test case

---
+++ src/hooks/useBoolean.test.ts
@@ -0,0 +1,34 @@
+import { act, renderHook } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+import { useBoolean } from './useBoolean'
+
+describe('useBoolean', () => {
+	it('should initialize with default value', () => {
+		const { result } = renderHook(() => useBoolean(true))
+		expect(result.current.value).toBe(true)
+	})
+
+	it('should set value to true', () => {
+		const { result } = renderHook(() => useBoolean())
+		act(() => {
+			result.current.setTrue()
+		})
+		expect(result.current.value).toBe(true)
+	})
+
+	it('should set value to false', () => {
+		const { result } = renderHook(() => useBoolean(true))
+		act(() => {
+			result.current.setFalse()
+		})
+		expect(result.current.value).toBe(false)
+	})
+
+	it('should toggle value', () => {
+		const { result } = renderHook(() => useBoolean(true))
+		act(() => {
+			result.current.toggle()
+		})
+		expect(result.current.value).toBe(false)
+	})
+})
--- src/hooks/useBoolean.ts
+++ src/hooks/useBoolean.ts
@@ -8,11 +8,34 @@ interface UseBooleanOutput {
 	toggle: () => void
 }
 
+/**
+ * Hook that provides boolean state with utility functions.
+ *
+ * @param {boolean} [defaultValue] - The initial value for the boolean state.
+ * @returns {UseBooleanOutput} - Object containing the boolean value and utility functions.
+ */
 export function useBoolean(defaultValue?: boolean): UseBooleanOutput {
 	const [value, setValue] = useState(!!defaultValue)
 
+	/**
+	 * Sets the boolean value to `true`.
+	 *
+	 * @type {() => void}
+	 */
 	const setTrue = useCallback(() => setValue(true), [])
+
+	/**
+	 * Sets the boolean value to `false`.
+	 *
+	 * @type {() => void}
+	 */
 	const setFalse = useCallback(() => setValue(false), [])
+
+	/**
+	 * Toggles the boolean value.
+	 *
+	 * @type {() => void}
+	 */
 	const toggle = useCallback(() => setValue((x) => !x), [])
 
 	return { value, setValue, setTrue, setFalse, toggle }
